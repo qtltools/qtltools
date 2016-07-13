df9196fc0fe6705a45146471aaa7f512a4e5ab73
odelaneau <olivier.delaneau@gmail.com>
2016-07-14 00:04:59 +0200

    Update Ubuntu12.04 package

--- script/plotTrans.R
+++ script/plotTrans.R
@@ -1,13 +1,17 @@
 #Read command line arguments
 args <- commandArgs(trailingOnly = TRUE)
-try(if(length(args) < 2) stop("Incorrect number of arguments, usage> Rscript plotTrans.R output.pdf nominals.txt permutation1.txt ... permutationN.txt"))
+try(if(length(args) < 3) stop("Incorrect number of arguments, usage> Rscript plotTrans.R output.pdf nominals.txt permutation1.txt ... permutationN.txt"))
 
 N=read.table(args[2], head=FALSE, stringsAsFactors=FALSE)
 N=N[order(N$V7), ]
 N$logpva = -log10(N$V7)
 
+P=read.table(args[3], head=FALSE, stringsAsFactors=FALSE)
+P=P[order(P$V7), ]
+P$logpva = -log10(P$V7)
+
 pdf(args[1], 6, 6)
-plot(0, 0, type="n", main="QQplot", xlab="Expected -log10(P-value)", ylab="Observed -log10(P-value)", xlim=c(min(N$logpva), max(N$logpva)+1), ylim=c(min(N$logpva), max(N$logpva)+1))
+plot(0, 0, type="n", main="QQplot", xlab="Expected -log10(P-value)", ylab="Observed -log10(P-value)", xlim=c(min(P$logpva), max(P$logpva)+1), ylim=c(min(N$logpva), max(N$logpva)+1))
 for (p in 3:length(args)) {
 	P=read.table(args[p], head=FALSE, stringsAsFactors=FALSE)
 	P=P[order(P$V7), ]
