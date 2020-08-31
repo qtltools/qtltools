b87d78ce4f95c6f7f30b28d82196ac0f8e052cd3
hongen <halitongen@gmail.com>
2020-08-31 15:36:45 +0200

    Improve check_for_updates.sh

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-RM=$(git remote update)
+RM=$(git remote update 2>&1)
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
@@ -12,6 +12,9 @@ elif [ $LOCAL = $BASE ]; then
     echo "Need to pull, please run git pull and recompile!"
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
+    SHORT=$(git rev-parse  --short=10 HEAD)
+    echo "Change log:"
+    git log $SHORT\..origin/HEAD --oneline --decorate --color --abbrev=10
 else
     echo "Diverged"
 fi
