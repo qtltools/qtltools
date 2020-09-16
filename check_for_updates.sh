8b29c0aa04a236f7c558011fb910cf5575c81752
halitongen <halitongen@gmail.com>
2020-09-16 11:33:08 +0200

    change the check for update script

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -10,6 +10,9 @@ if [ $LOCAL = $REMOTE ]; then
     echo "Up-to-date"
 elif [ $LOCAL = $BASE ]; then
     echo "Need to pull, please run git pull and recompile!"
+    SHORT=$(git rev-parse  --short=10 HEAD)
+    echo "Change log:"
+    git log $SHORT\..origin/HEAD --oneline --decorate --color --abbrev=10
 elif [ $REMOTE = $BASE ]; then
     echo "Need to push"
     SHORT=$(git rev-parse  --short=10 HEAD)
